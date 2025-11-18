6869763f793a841bd84d0b087ac5c801c8233aea
Ryo Washizu <wasizuryo@gmail.com>
2025-11-18 14:22:41 +0900

    問題に成功した場合と，不正解の場合のテストを追加

--- src/main.test.js
+++ src/main.test.js
@@ -45,3 +45,91 @@ describe('LaunchRequestを起動して最初の問題を出題', () => {
     })
   });
 });
+
+describe("問題に成功した場合", () => {
+  let speechResponse;
+
+  before(async() => {
+    const ctx = context();
+    const event = require('./fixtures/answer.json');
+
+    const getNextItemIndex = () => 4;
+    const handler = index.createHandler(getNextItemIndex)
+    handler(event, ctx)
+
+    try {
+      speechResponse = await ctx.Promise
+    } catch (error) {
+      console.log("Error", error)
+    }
+  })
+
+  it("handlerのresponse", () => {
+    assert.deepEqual(speechResponse, {
+      "version": "1.0",
+      "response": {
+        "outputSpeech": {
+          "ssml": "<speak> そうです。では2問目。栃木県の県庁所在地は？ </speak>",
+          "type": "SSML"
+        },
+        "reprompt": {
+          "outputSpeech": {
+            "ssml": "<speak> 2問目。栃木県の県庁所在地は？ </speak>",
+            "type": "SSML"
+          }
+        },
+        "shouldEndSession": false
+      },
+      "sessionAttributes": {
+        "advance": 2,
+        "itemIndex": 4,
+        "score": 1
+      },
+      "userAgent": "ask-nodejs/1.0.25 Node/v22.19.0",
+    })
+  })
+})
+
+describe("問題に不正解の場合", () => {
+  let speechResponse;
+
+  before(async() => {
+    const ctx = context();
+    const event = require('./fixtures/bad_answer.json');
+
+    const getNextItemIndex = () => 4;
+    const handler = index.createHandler(getNextItemIndex)
+    handler(event, ctx)
+
+    try {
+      speechResponse = await ctx.Promise
+    } catch (error) {
+      console.log("Error", error)
+    }
+  })
+
+  it("handlerのresponse", () => {
+    assert.deepEqual(speechResponse, {
+      "version": "1.0",
+      "response": {
+        "outputSpeech": {
+          "ssml": "<speak> ちがいます。正解は8です。では2問目。栃木県の県庁所在地は？ </speak>",
+          "type": "SSML"
+        },
+        "reprompt": {
+          "outputSpeech": {
+            "ssml": "<speak> 2問目。栃木県の県庁所在地は？ </speak>",
+            "type": "SSML"
+          }
+        },
+        "shouldEndSession": false
+      },
+      "sessionAttributes": {
+        "advance": 2,
+        "itemIndex": 4,
+        "score": 0
+      },
+      "userAgent": "ask-nodejs/1.0.25 Node/v22.19.0",
+    })
+  })
+})
